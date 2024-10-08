20a5c1e82cf2001eadb94a5406f5a8e2aa6a1dd1
deca-francesco <francescodecarlo70@gmail.com>
2024-10-08 17:41:31 +0200

    ADD: img toggle on button click

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,20 @@
 console.log("js linked");
 
-// prendo il bottone tramite id
-document.getElementById("lamp_btn").addEventListener("click", function() {
-    document.getElementById("")
+// prendo il bottone e le immagini tramite id
+const lamp_btnEl = document.getElementById("lamp_btn");
+const lamp_off_imgEl = document.getElementById("lamp_off_img");
+const lamp_on_imgEl = document.getElementById("lamp_on_img");
+
+lamp_btnEl.addEventListener("click", function() {
+    // faccio sparire o comparire la prima immagine
+    lamp_off_imgEl.classList.toggle("d-none");
+    // faccio comparire o sparire la seconda immagine, ma al contrario della prima
+    lamp_on_imgEl.classList.toggle("d-none");
+
+    // modifico la scritta del bottone
+    if (lamp_btnEl.innerText.includes("Accendi")) {
+        lamp_btnEl.innerText.replace("Accendi", "Spegni");
+    } else if (lamp_btnEl.innerText.includes("Spegni")) {
+        lamp_btnEl.innerText.replace("Spegni", "Accendi")
+    }
 });
